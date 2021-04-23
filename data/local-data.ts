1059be642e52aeb1c1a4d500514f9b496af8b920
Ernest Irabor <irabeny89@gmail.com>
2021-04-23 09:37:16 +0100

    local data initial commit

---
+++ data/local-data.ts
@@ -0,0 +1,30 @@
+const localData = {
+  appName: "DC",
+  game: {
+    name: "DC Lucky Picker",
+    instruction: {
+      title: "Instructions",
+      inGameTitle: "Pick a card",
+      p1: "Hi, play the game below and try to get at least 4 points out of 10 chances.",
+      p2: "Pick a card and you score 1 point when you find the DC character hidden behind one of the cards only.",
+    },
+    chanceLimit: 10,
+    optionLimit: 4,
+    images: [
+      {
+        src: "/supman-logo-color.jpg",
+        width: "150",
+        height: "150",
+        alt: "superman logo color"
+      },
+      {
+        src: "/supman-lego.jpg",
+        width: "150",
+        height: "150",
+        alt: "superman lego"
+      }
+    ]
+  },
+}
+
+export default localData
