ed758e6571a4227967822977584607ab20575e1a
Ernest Irabor <irabeny89@gmail.com>
2021-04-23 09:38:26 +0100

    helper function initial commit

---
+++ components/Layout.tsx
@@ -0,0 +1,17 @@
+import { FC } from "react"
+
+const Layout: FC = ({ children }) => (
+	<div>
+		<header className="bg-primary text-white p-1 font-italic">
+			<h1>DC</h1>
+		</header>
+		<main className="p-2" style={{
+			minHeight: "100vh",
+			maxWidth: 400,
+			margin: "0 auto"
+		}}>{children}</main>
+		<footer className="bg-secondary text-white pl-1">&copy; 2021</footer>
+	</div>
+)
+
+export default Layout
