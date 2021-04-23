ec8f30f527908956f3f380fa84e9132e3c03826d
Ernest Irabor <irabeny89@gmail.com>
2021-04-23 21:44:22 +0100

    initial commit

--- data/local-data.ts
+++ data/local-data.ts
@@ -1,30 +1,72 @@
 const localData = {
-  appName: "DC",
-  game: {
-    name: "DC Lucky Picker",
-    instruction: {
-      title: "Instructions",
-      inGameTitle: "Pick a card",
-      p1: "Hi, play the game below and try to get at least 4 points out of 10 chances.",
-      p2: "Pick a card and you score 1 point when you find the DC character hidden behind one of the cards only.",
-    },
-    chanceLimit: 10,
-    optionLimit: 4,
-    images: [
-      {
-        src: "/supman-logo-color.jpg",
-        width: "150",
-        height: "150",
-        alt: "superman logo color"
-      },
-      {
-        src: "/supman-lego.jpg",
-        width: "150",
-        height: "150",
-        alt: "superman lego"
-      }
-    ]
-  },
+	appName: "DC",
+	game: {
+		name: "DC Lucky Picker",
+		instruction: {
+			title: "Instructions",
+			inGameTitle: "Pick a card",
+			p1:
+				"Hi, play the game below and try to get at least 6 points out of 10 chances.",
+			p2:
+				"Pick a card and you score 1 point when you find the DC character hidden behind one of the cards only.",
+		},
+		chanceLimit: 10,
+		passLimit: 6,
+		optionLimit: 4,
+		themes: [
+			{
+				character: "Superman",
+				images: [
+					{
+						src: "/supman-logo-color.jpg",
+						width: "150",
+						height: "150",
+						alt: "superman logo color",
+					},
+					{
+						src: "/supman.png",
+						width: "150",
+						height: "150",
+						alt: "superman",
+					},
+				],
+			},
+			{
+				character: "Batman",
+				images: [
+					{
+						src: "/batman-logo.png",
+						width: "150",
+						height: "150",
+						alt: "batman logo",
+					},
+					{
+						src: "/batman.jpg",
+						width: "150",
+						height: "150",
+						alt: "batman",
+					},
+				],
+			},
+			{
+				character: "Flash",
+				images: [
+					{
+						src: "/flash-logo.png",
+						width: "150",
+						height: "150",
+						alt: "flash logo",
+					},
+					{
+						src: "/flash.jpg",
+						width: "150",
+						height: "150",
+						alt: "flash",
+					},
+				],
+			},
+		],
+	},
 }
 
 export default localData
---
+++ pages/index.tsx
@@ -0,0 +1,233 @@
+import Image from "next/Image"
+import { useReducer, SyntheticEvent } from "react"
+import { Button, Badge, Modal } from "react-bootstrap"
+import localData from "./../data/local-data"
+import { gameInitialState } from "../data/initialStates"
+import { gameReducer, CORRECT, WRONG, RESTART } from "../lib/reducers"
+import confirmPick from "./../lib/confirmPick"
+
+const theme = localData.game.themes[confirmPick(localData.game.themes.length) - 1]
+
+const Home = () => {
+	const [
+		{ score, chance, is1correct, is2correct, is3correct, is4correct, comment },
+		gameDispatch,
+	] = useReducer(gameReducer, gameInitialState)
+	const {
+		name,
+		instruction,
+		optionLimit,
+		chanceLimit,
+		passLimit,
+	} = localData.game
+  const { images } = theme
+	const cardImage = images[0]
+	const targetImage = images[1]
+
+	const handleClick = ({ currentTarget: { id } }: SyntheticEvent) => {
+		document.getElementById(id)!.style.transition = "transform 100ms"
+		document.getElementById(id)!.style.transform = "scale(1.1)"
+		const correctPick = confirmPick(optionLimit)
+		if (chance! > 0) {
+			if (parseInt(id) !== correctPick) {
+				gameDispatch({
+					type: WRONG,
+					payload: { chance: chance! - 1, comment: "Wrong!" },
+				})
+			} else {
+				gameDispatch({
+					type: CORRECT,
+					payload: {
+						score: score! + 1,
+						chance: chance! - 1,
+						[`is${id}correct`]: true,
+						comment: "Correct!",
+					},
+				})
+				setTimeout(() => {
+					gameDispatch({
+						type: CORRECT,
+						payload: { [`is${id}correct`]: false },
+					})
+				}, 1000)
+			}
+		}
+		setTimeout(
+			() => (document.getElementById(id)!.style.transform = "scale(1)"),
+			1000,
+		)
+	}
+	const restart = () =>
+		gameDispatch({ type: RESTART, payload: gameInitialState })
+	return (
+		<div className="text-warning p-2" style={{ maxWidth: 400 }}>
+			<div>
+				<div
+					className="rounded px-2 py-1"
+					style={{
+						backgroundColor: "rgba(0, 0, 0, 0.50)",
+					}}>
+					<h2>{name}</h2>
+					<hr />
+					<h3>{instruction.title}</h3>
+					<div className="text-white">
+						<p>{instruction.p1}</p>
+						<p>{instruction.p2}</p>
+					</div>
+				</div>
+			</div>
+			<div className="d-flex justify-content-between p-2 rounded mb-2">
+				<div className="rounded bg-secondary p-2 my-2 h3">
+					Score:{" "}
+					<Badge variant="success" pill>
+						{score}
+					</Badge>
+				</div>
+				<div className="bg-secondary rounded p-2 my-2 h3">
+					<Badge pill variant="danger">
+						{chance}
+					</Badge>{" "}
+					Chances
+				</div>
+			</div>
+			<div>
+				<div className="d-flex justify-content-between">
+					<h3 className="bg-info p-1 rounded">Pick a card</h3>
+					{chance! > 0 ? (
+						<h3
+							className={`${
+								comment === "Correct!" ? "text-success" : "text-danger"
+							} bg-white p-1 rounded`}>
+							{comment}
+						</h3>
+					) : (
+						<h3 className="bg-danger px-2 rounded">Game Over!</h3>
+					)}
+				</div>
+				<div
+					style={{
+						maxWidth: 320,
+						margin: "0 auto",
+					}}>
+					<div className="d-flex justify-content-around">
+						{is1correct ? (
+							<div>
+								<Image
+									src={targetImage.src}
+									width={targetImage.width}
+									height={targetImage.height}
+									alt={targetImage.alt}
+								/>
+							</div>
+						) : (
+							<div>
+								<Image
+									src={cardImage.src}
+									width={cardImage.width}
+									height={cardImage.height}
+									alt={cardImage.alt}
+									id="1"
+									onClick={handleClick}
+								/>
+							</div>
+						)}
+						{is2correct ? (
+							<div>
+								<Image
+									src={targetImage.src}
+									width={targetImage.width}
+									height={targetImage.height}
+									alt={targetImage.alt}
+								/>
+							</div>
+						) : (
+							<div>
+								<Image
+									src={cardImage.src}
+									width={cardImage.width}
+									height={cardImage.height}
+									alt={cardImage.alt}
+									id="2"
+									onClick={handleClick}
+								/>
+							</div>
+						)}
+					</div>
+					<div className="d-flex justify-content-around">
+						{is3correct ? (
+							<div>
+								<Image
+									src={targetImage.src}
+									width={targetImage.width}
+									height={targetImage.height}
+									alt={targetImage.alt}
+								/>
+							</div>
+						) : (
+							<div>
+								<Image
+									src={cardImage.src}
+									width={cardImage.width}
+									height={cardImage.height}
+									alt={cardImage.alt}
+									id="3"
+									onClick={handleClick}
+								/>
+							</div>
+						)}
+						{is4correct ? (
+							<div>
+								<Image
+									src={targetImage.src}
+									width={targetImage.width}
+									height={targetImage.height}
+									alt={targetImage.alt}
+								/>
+							</div>
+						) : (
+							<div>
+								<Image
+									src={cardImage.src}
+									width={cardImage.width}
+									height={cardImage.height}
+									alt={cardImage.alt}
+									id="4"
+									onClick={handleClick}
+								/>
+							</div>
+						)}
+					</div>
+				</div>
+			</div>
+			{chance! === 0 && (
+				<Modal
+					size="sm"
+					show={true}
+					centered
+					backdrop="static"
+					keyboard={false}>
+					<Modal.Header>
+						<h4>Your result...</h4>
+					</Modal.Header>
+					<Modal.Body>
+						<h5>Total Score: {score}</h5>
+						<h5>Total Miss: {chanceLimit - score!}</h5>
+						<hr />
+						{score! >= passLimit ? (
+							<h5>Congratulations! You passed!</h5>
+						) : (
+							<h5>You did not pass; try again below.</h5>
+						)}
+					</Modal.Body>
+					<Modal.Footer>
+						<Button size="lg" onClick={restart} block className="my-3">
+							<h3>Restart</h3>
+						</Button>
+					</Modal.Footer>
+				</Modal>
+			)}
+		</div>
+	)
+}
+
+export default Home
